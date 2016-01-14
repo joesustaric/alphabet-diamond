e0d6600d0d37d56b35a0c19ec561e958d1c9ad30
Joe Sustaric <joe8307@gmail.com>
2016-01-14 00:01:11 +0000

    figuring out the letter combos from the given input

--- main.go
+++ main.go
@@ -8,10 +8,30 @@ import (
 )
 
 var letter *string
+var alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
 const (
 	inputStringSize = 1
+	diamond         = `
+{{.First}}
+{{.Second}} {{.Second}}
+{{.Third}} {{.Fourth}}
+{{.Fifth}} {{.Fifth}}
+{{.Sixth}} {{.Sixth}}
+{{.Fifth}} {{.Fifth}}
+{{.Third}} {{.Fourth}}
+{{.Second}} {{.Second}}
+{{.First}}
+`
 )
+
+type Letters struct {
+	First, Second, Third, Fourth, Fifth string
+}
+
+func NewLetters(first, second, third, fourth, fifth string) Letters {
+	return Letters{First: first, Second: second, Third: third, Fourth: fourth, Fifth: fifth}
+}
 
 func init() {
 	letter = flag.String("letter", "", "The letter which is the middle of the diamond.")
@@ -19,19 +39,56 @@ func init() {
 }
 
 func main() {
-	fmt.Printf("letter = %s \n", *letter)
+
+	if input, err := Parse(*letter); err != nil {
+		fmt.Println(err.Error())
+	} else {
+		fmt.Printf("letter = %s \n", input)
+	}
+
 }
 
 func Parse(input string) (string, error) {
 	r := strings.TrimSpace(input)
 
 	if notCorrectLength(r) {
-		return "", fmt.Errorf("too many characters %s", r)
+		return "", fmt.Errorf("input incorrect %s", r)
 	} else if notAlphabetCharacter(r) {
 		return "", fmt.Errorf("not a-z or A-Z character")
 	}
 
 	return strings.ToUpper(r), nil
+}
+
+func GetDiamondLetters(seed string) Letters {
+
+	r := strings.Split(alphabet, seed)
+
+	var letters []string
+	count := 0
+	letters = append(letters, seed)
+
+	for i := len(r[0]) - 1; i >= 0; i-- {
+		letters = append(letters, string(r[0][i]))
+		count++
+		if count == 4 {
+			break
+		}
+	}
+
+	if len(letters) != 4 {
+		for i := len(r[1]) - 1; i > 0; i-- {
+			letters = append(letters, string(r[1][i]))
+			count++
+			if count == 4 {
+				break
+			}
+
+		}
+	}
+
+	return NewLetters(letters[4], letters[3], letters[2], letters[1], letters[0])
+
 }
 
 func notCorrectLength(input string) bool {
--- main_test.go
+++ main_test.go
@@ -52,5 +52,42 @@ func TestParse(t *testing.T) {
 				test.input)
 		}
 	}
+}
 
+func TestGetLettersForTheDiamond(t *testing.T) {
+
+	var happyTests = []struct {
+		input    string
+		expected Letters
+	}{
+		{"E", Letters{First: "A", Second: "B", Third: "C", Fourth: "D", Fifth: "E"}},
+		{"A", Letters{First: "W", Second: "X", Third: "Y", Fourth: "Z", Fifth: "A"}},
+		{"B", Letters{First: "X", Second: "Y", Third: "Z", Fourth: "A", Fifth: "B"}},
+		{"Z", Letters{First: "V", Second: "W", Third: "X", Fourth: "Y", Fifth: "Z"}},
+	}
+
+	for _, test := range happyTests {
+		result := GetDiamondLetters(test.input)
+		if areNotEqual(result, test.expected) {
+			t.Errorf("Fail Input=%s ,expected=%s ,result=%v \n",
+				test.input, test.expected, result)
+		}
+	}
+
+}
+
+func areNotEqual(result Letters, expected Letters) bool {
+	if result.First != expected.First {
+		return true
+	} else if result.Second != expected.Second {
+		return true
+	} else if result.Third != expected.Third {
+		return true
+	} else if result.Fourth != expected.Fourth {
+		return true
+	} else if result.Fifth != expected.Fifth {
+		return true
+	}
+
+	return false
 }
